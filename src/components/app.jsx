2b2874799c3cb0d46182abcef0246d5a08c815bf
hkl-iii <rahleggon@gmail.com>
2021-05-18 20:43:25 -0500

    modified appjsx, pg 19 tutorial

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
 
 class App extends Component {
-    state = { } 
+   constructor(props) {
+       super(props);
+       this.books = [
+        {title: 'Ready Player One', author: 'Ernest Cline'},
+        {title: 'All the Light We Cannot See', author: 'Anthony Doerr'},
+        {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurti'},
+       ];
+       this.state = {
+           bookNumber: 0
+       }
+   }
     render() { 
         return (
             <div className="container-fluid">
                <TitleBar /> 
+               <div className="row row-spacer">
+                   <div className="col">
+                       {/*Button here to move to the previous book viewed*/}
+                   </div>
+                   <div className="col">
+                       {/*Display Book with Cover here*/}
+                   </div>
+                   <div className="col">
+                       {/*Button here to move to the previous book viewed*/}
+                   </div>
+               </div>
             </div>
         
         );
