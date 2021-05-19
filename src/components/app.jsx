1ddb606a03458d73ac87f47a1bc7798ee2a528e2
hkl-iii <rahleggon@gmail.com>
2021-05-18 19:48:51 -0500

    added new component from tutorial

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TitleBar from './TitleBar/titleBar';
+
+class App extends Component {
+    state = { } 
+    render() { 
+        return (
+            <div className="container-fluid">
+               <TitleBar /> 
+            </div>
+        
+        );
+    }
+}
+
+export default App;
